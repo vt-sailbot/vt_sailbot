b089e7986b0cee3ec28aa6d955f27c01241d71c7
Animated <73669160+ChrisNassif@users.noreply.github.com>
2023-11-09 08:27:35 -0500

    init

---
+++ build_all_images.sh
@@ -0,0 +1,3 @@
+docker build -t sailbot_wind_sensor --build-arg NODE_NAME=wind_sensor .
+docker build -t sailbot_sailcode --build-arg NODE_NAME=sailcode .
+docker build -t sailbot_telemetry --build-arg NODE_NAME=telemetry .
