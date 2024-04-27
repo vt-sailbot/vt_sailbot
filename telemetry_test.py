955315570c62c445adb31e06c5a2406e26c15ee6
Chris Nassif <chrisjnassif@gmail.com>
2024-04-27 00:11:24 -0400

    Updating References

---
+++ telemetry_test.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import time
+import os
+
+TELEMETRY_SERVER_URL = 'http://54.145.37.169:8082/'
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    
+while True:
+    
+    telemetry_list = json.loads(requests.get(TELEMETRY_SERVER_URL + "api/").text)
+    telemetry = telemetry_list[-1]["s1"].split("; ")
+    gps_lat_lon = telemetry[0].split(", ")
+    next_destination_lat_lon = telemetry[5].split(", ")
+    
+    clear_screen()
+    
+    print(f"GPS Latitude: {gps_lat_lon[0]}, GPS Longitude: {gps_lat_lon[1]}")
+    print(f"Speed Over Ground (m/s): {telemetry[1]}")
+    print(f"Heading: {telemetry[2]}")
+    print(f"Wind Speed (m/s): {telemetry[3]}, Wind Direction: {telemetry[4]}")
+    print(f"Next Waypoint Latitude: {next_destination_lat_lon[0]}, Next Waypoint Longitude: {next_destination_lat_lon[1]}")
+    print(f"Desired Mast Angle: {telemetry[6]}")
+    print(f"Desired Rudder Angle: {telemetry[7]}")
+    
+    # telemetry_string = "hi there"
+    # print(requests.post(url= TELEMETRY_SERVER_URL + "api/", json={"s1": telemetry_string, "s2": "1"}).text)
+    
+    time.sleep(1)
