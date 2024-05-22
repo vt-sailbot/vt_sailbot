681f6f510fe39f091bd864d4fc3c125be826102b
Chris Nassif <chrisjnassif@gmail.com>
2024-05-22 06:54:35 -0400

    Updating References

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,4 +1,4 @@
-docker build -t sailbot_event_manageer --build-arg NODE_NAME=sailbot_event_manager .
+docker build -t sailbot_event_manager --build-arg NODE_NAME=sailbot_event_manager .
 docker build -t sailbot_autopilot --build-arg NODE_NAME=autopilot .
 docker build -t sailbot_telemetry --build-arg NODE_NAME=telemetry .
 docker build -t sailbot_mcu --build-arg NODE_NAME=mcu .
