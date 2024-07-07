23607ee02ebbc13993f94aa3ac5eef3e9f0a9f33
Chris Nassif <chrisjnassif@gmail.com>
2024-07-07 08:37:46 +0000

    part 1 big reorganization. Repo is not the ros workspace instead of the src directory

---
+++ .devcontainer/docker_dev_environment_setup.sh
@@ -0,0 +1,19 @@
+# This is very unsafe lol but I need this to be able to access docker from inside of the dev container for the sim. 
+# Do not remove unless we are releasing this software as a product
+echo sudo chmod 777 /var/run/docker.sock >> "/home/sailbot_user/.bashrc"
+echo sudo chmod -R 777 /home/ >> "/home/sailbot_user/.bashrc"
+
+pip install -r requirements.txt
+
+source /opt/ros/humble/setup.bash
+
+echo source /opt/ros/humble/setup.bash >> "/home/sailbot_user/.bashrc"
+source /home/sailbot_user/.bashrc
+
+colcon build
+
+echo source /home/ws/install/setup.bash >> "/home/sailbot_user/.bashrc"
+source /home/sailbot_user/.bashrc
+
+colcon build
+
